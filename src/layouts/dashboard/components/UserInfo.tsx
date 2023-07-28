5ef60c5be9938ce0f259d7c0e84a4c56c7534cf7
PromiseUdo <info.promiseudo@gmail.com>
2023-07-28 14:19:04 +0100

    UI fixes on the header

--- src/layouts/dashboard/components/UserInfo.tsx
+++ src/layouts/dashboard/components/UserInfo.tsx
@@ -2,7 +2,7 @@ import Typography from "@/common/Typography";
 
 const UserInfo = () => {
   return (
-    <div className="mb-4 w-full flex border-t border-contact-shades-gray-1 pt-6 gap-2.5 h-[62px] cursor-pointer items-center sm:pt-0 sm:border-none sm:w-auto sm:min-w-[130px] md:justify-end lg:gap-2.5 lg:border-t lg:h-[62px] lg:p-6 lg:pb-0 lg:w-full lg:border-contact-shades-gray-1 lg:justify-start">
+    <div className="mb-8 w-full flex border-t border-contact-shades-gray-1 pt-6 gap-2.5 h-[62px] cursor-pointer items-center sm:pt-0 sm:border-none sm:w-auto sm:min-w-[130px] md:justify-end lg:gap-2.5 lg:border-t lg:h-[62px] lg:p-6 lg:pb-0 lg:w-full lg:border-contact-shades-gray-1 lg:justify-start">
       <div className="w-8 h-8 rounded-full border flex justify-center items-center relative">
         <img
           src="/defaultImage.png"
