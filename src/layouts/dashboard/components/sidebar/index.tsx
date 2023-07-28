537ac3561dc43ebb23e3c32abad1a83fe03f5212
PromiseUdo <info.promiseudo@gmail.com>
2023-07-28 14:02:59 +0100

    update react-leaflet container zindex

--- src/layouts/dashboard/components/sidebar/index.tsx
+++ src/layouts/dashboard/components/sidebar/index.tsx
@@ -6,7 +6,7 @@ import UserInfo from "../UserInfo";
 const Sidebar = () => {
   return (
     <>
-      <div className="border-r top-4  border-contact-shades-gray-1 h-screen bg-contact-shades-white flex flex-col justify-between pt-8 px-0 gap-20 min-w-full">
+      <div className="z-20 border-r top-4  border-contact-shades-gray-1 h-screen bg-contact-shades-white flex flex-col justify-between pt-8 px-0 gap-20 min-w-full">
         <div className="flex flex-col justify-between px-0 gap-20 min-w-full">
           <Link to={"/"} className="h-[39px]">
             <img src="/contact-logo.png" alt="logo" className="h-16 w-16" />
